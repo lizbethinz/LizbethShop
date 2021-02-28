8dd241cef06b65ae93c1958a7dd9f9c847a52ecb
Lizbeth Inzunza <lizinzunza0@gmail.com>
2021-02-28 12:45:45 -0700

    primer commit

---
+++ src/main/java/com/Tienda/LizbethShop/LizbethShopApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda.LizbethShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizbethShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LizbethShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda/LizbethShop/LizbethShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda.LizbethShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizbethShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
